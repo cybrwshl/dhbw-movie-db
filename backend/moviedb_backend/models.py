94ee1fdaf01d18d720d19d11210651d152d7681e
cybrwushl93 <cybrwushl93@bitground.de>
2015-05-07 15:40:27 +0200

    moved models removed frontend

--- backend/moviedb_backend/models.py
+++ backend/moviedb_backend/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    birthday = models.DateField()
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=50)
+    budget = models.DecimalField()
+    overview = models.CharField(max_length=500)
+
+
+class Keyword(models.Model):
+    name = models.CharField(max_length=30)
+    movie = models.ForeignKey(Movie)
+
+
+class Role(models.Model):
+    ROLE_TYPES = (
+        ('A', 'Actor'),
+        ('P', 'Producer'),
+        ('W', 'Writer'),
+    )
+    type = models.CharField(max_length=1, choices=ROLE_TYPES, blank=False)
+    movie = models.ForeignKey(Movie)
+    person = models.ForeignKey(Person)
+    additional_info = models.CharField(max_length=30)
